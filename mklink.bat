8201987dcb79c0b351c1f551804b48003af0dabd
takuya711 <s.takuya93@gmail.com>
2015-01-23 04:10:15 +0900

    add texstudio settings

--- mklink.bat
+++ mklink.bat
@@ -1,5 +1,6 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
 mklink %HOMEPATH%"\.latexmkrc" %HOMEPATH%"\dotfiles\.latexmkrc"
+mklink %HOMEPATH%"\AppData\Roaming\texstudio\takuya.txsprofile" %HOMEPATH%"\dotfiles\takuya.txsprofile"
 mklink %HOMEPATH%"\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" %HOMEPATH%"\dotfiles\Microsoft.PowerShell_profile.ps1"
 mklink /D %HOMEPATH%"\vimfiles\colors" %HOMEPATH%"\dotfiles\colors"
 mklink /D %HOMEPATH%"\scripts" %HOMEPATH%"\dotfiles\scripts"
